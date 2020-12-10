e7e18ef736418719f48a13053c89300832ffdba4
MkTverHub <vladimijump26@gmail.com>
2020-12-11 00:02:33 +0300

    смсмсмссмс

--- basicFins/src/main/java/com/sweetcard/basic/dao/repository/FinancedataRepository.java
+++ basicFins/src/main/java/com/sweetcard/basic/dao/repository/FinancedataRepository.java
@@ -1,11 +1,7 @@
 package com.sweetcard.basic.dao.repository;
 
-import com.sweetcard.basic.dao.entities.Customer;
 import com.sweetcard.basic.dao.entities.Financedata;
-import com.sweetcard.basic.dao.entities.Finsproject;
-import org.hibernate.annotations.SQLUpdate;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -37,5 +33,9 @@ public interface FinancedataRepository extends JpaRepository<Financedata, Intege
     //Получить расход по проекту
     @Query(value = "select sum(amount) as expense from financedata where project_id = :fins_project_id and fins_oper_type = 'expense'", nativeQuery = true)
     String GetProjectExpense (@Param("fins_project_id") Integer finsprojectid);
+
+    //----Отчетность----------
+    //Получить profit по годам
+
 
 }
