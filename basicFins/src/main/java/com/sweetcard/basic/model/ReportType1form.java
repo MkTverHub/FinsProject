ac79da8c42f0677a82a23f76911f35042a4657e4
MkTverHub <vladimijump26@gmail.com>
2020-12-10 23:09:33 +0300

    смсмсмссмс

---
+++ basicFins/src/main/java/com/sweetcard/basic/model/ReportType1form.java
@@ -0,0 +1,14 @@
+package com.sweetcard.basic.model;
+
+public class ReportType1form {
+    private String strValue1 = "";
+    private String strValue2 = "";
+
+    /*----------------GET / SET Полей--------------*/
+    /*Имя get/set должно начинать с get/set + имя атрибута в html (с у четом заглавных)*/
+    public String getValue1() {return strValue1;}
+    public void setValue1(String Value1) {this.strValue1 = Value1;}
+    public String getValue2() {return strValue2;}
+    public void setValue2(String Value2) {this.strValue2 = Value2;}
+
+}
