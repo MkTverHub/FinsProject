e5553e358cf338c63a6919b651a58d286a9855a7
MkTverHub <vladimijump26@gmail.com>
2020-12-25 00:45:48 +0300

    24.12.2020

---
+++ basicFins/src/main/java/com/sweetcard/basic/dao/repository/AggregateDataRepository.java
@@ -0,0 +1,14 @@
+package com.sweetcard.basic.dao.repository;
+
+import com.sweetcard.basic.dao.entities.AggregateData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+
+
+public interface AggregateDataRepository extends JpaRepository<AggregateData, Integer> {
+    @Query(value = "select cnt.id, cnt.name from contragent cnt left join financedata fd on cnt.id = fd.finscontragent", nativeQuery = true)
+    List<AggregateData> GetAll();
+}
