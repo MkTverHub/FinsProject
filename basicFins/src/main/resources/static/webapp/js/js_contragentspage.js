5f2b37a5ef84163ee315442d7c7ec1a32f5b8390
MkTverHub <vladimijump26@gmail.com>
2020-12-27 15:26:17 +0300

    27.12.2020

--- basicFins/src/main/resources/static/webapp/js/js_contragentspage.js
+++ basicFins/src/main/resources/static/webapp/js/js_contragentspage.js
@@ -477,3 +477,34 @@ function ResetPickList(ListSelector){
     $(ListSelector).find('[selected]').attr('select_value', null);
 
 }
+
+//=========================================================================================================
+//Форматировать числа в полях (вызывается при загрузки Fins_Contragents_Add.html)
+function SetFormatData(selectop_in){
+    let elements = document.querySelectorAll(selectop_in);
+    for (let elem of elements) {
+        elem.innerHTML = number_format(elem.innerHTML, 2, '.', ' ');
+    }
+}
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function (n, prec) {
+            var k = Math.pow(10, prec);
+            return '' + Math.round(n * k) / k;
+        };
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+}
