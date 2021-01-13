b0ac6009c772fb2fdc6ff2813bb363ba918a2740
MkTverHub <vladimijump26@gmail.com>
2021-01-13 21:44:57 +0300

    13.01.2021

--- basicFins/src/main/resources/static/webapp/js/js_finsoperationpage.js
+++ basicFins/src/main/resources/static/webapp/js/js_finsoperationpage.js
@@ -3,7 +3,7 @@
 //Функция при загрузки страницы
 function StartPage() {
     //alert('StartPage');
-    doAjaxGetProjectList();//Получение списка проектов в левой панели
+    doAjaxGetProjectListLeft();//Получение списка проектов в левой панели
     doAjaxGetContragentsList();//Получение списка контрагентов
     SetROForm();//Сделать формк RO
 };
@@ -431,30 +431,6 @@ function UnSetROForm(){
 
 
 //-----------Ajax Functions------------
-//Ajax получение списка проектов в левой панели
-function doAjaxGetProjectList() {
-    $.ajax({
-        url : 'GetFinsProjectList',
-        type: 'GET',
-        dataType: 'json',
-        contentType: 'application/json',
-        mimeType: 'application/json',
-        data : ({
-
-        }),
-        success: function (data) {
-            var strProjectListContext = "";
-            var obj = jQuery.parseJSON(data.text);
-            $.each(obj, function (index, value) {
-                strProjectListContext = strProjectListContext
-                    + '<li id="' + value["id"].toString()
-                    + '_rowid" class="left-menu-item finsproject_list_row_li"><input type="button" class="left-menu-link finsproject_list_row" projnum="'
-                    + value["id"].toString() + '" value="' + value["name"] + '"/></li>';
-            });
-            $("#projectlistpanel").html(strProjectListContext);
-        }
-    });
-};
 
 //Ajax получение списка операций по проекту
 function doAjaxGetProjectOperationList(ProjectNum) {
