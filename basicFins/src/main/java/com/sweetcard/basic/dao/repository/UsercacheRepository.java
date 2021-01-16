677371f0758bcf138cdbe9e0b43e8651d6dd3379
MkTverHub <vladimijump26@gmail.com>
2021-01-17 00:02:47 +0300

    17.01.2021

---
+++ basicFins/src/main/java/com/sweetcard/basic/dao/repository/UsercacheRepository.java
@@ -0,0 +1,15 @@
+package com.sweetcard.basic.dao.repository;
+
+import com.sweetcard.basic.dao.entities.Financedata;
+import com.sweetcard.basic.dao.entities.Usercache;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UsercacheRepository extends JpaRepository<Usercache, Integer> {
+    //Выбрать все селектом по проекту
+    @Query(value = "SELECT id,active_proj,login FROM usercache where login = :user_login", nativeQuery = true)
+    Usercache GetUsercache(@Param("user_login") String UserLogin);
+}
