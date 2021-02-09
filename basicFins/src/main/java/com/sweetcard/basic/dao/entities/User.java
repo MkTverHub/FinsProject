40e5f1a2153a609d1084468ead5714b5111e9f25
MkTverHub <vladimijump26@gmail.com>
2021-02-09 21:21:25 +0300

    09.02.2021 2

--- basicFins/src/main/java/com/sweetcard/basic/dao/entities/User.java
+++ basicFins/src/main/java/com/sweetcard/basic/dao/entities/User.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.Size;
 import java.util.Collection;
 import java.util.Set;
 
@@ -17,9 +17,9 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min=2, message = "Не меньше 5 знаков")
+    //@Size(min=2, message = "Не меньше 5 знаков")
     private String username;
-    @Size(min=2, message = "Не меньше 5 знаков")
+   // @Size(min=2, message = "Не меньше 5 знаков")
     private String password;
     @Transient
     private String passwordConfirm;
