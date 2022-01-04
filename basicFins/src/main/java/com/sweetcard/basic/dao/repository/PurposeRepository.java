593d299f1ac6cddeecaf5176e128bb400be65893
MkTverHub <vladimijump26@gmail.com>
2022-01-05 00:33:15 +0300

    05.01.2022 Цели - архитектура 1

--- basicFins/src/main/java/com/sweetcard/basic/dao/repository/PurposeRepository.java
+++ basicFins/src/main/java/com/sweetcard/basic/dao/repository/PurposeRepository.java
@@ -12,6 +12,6 @@ import java.util.List;
 public interface PurposeRepository extends JpaRepository<Purpose, Integer> {
     String strSpecSymbol = "\\:";
     //По проекту
-    @Query(value = "select id,description,name,par_row_id,ROUND(AVG(expense)"+strSpecSymbol+strSpecSymbol+"numeric,2) as expense,ROUND(AVG(profit)"+strSpecSymbol+strSpecSymbol+"numeric,2) as profit from purpose where par_row_id = :fins_project_id group by id", nativeQuery = true)
+    @Query(value = "select id,description,name,par_row_id,ROUND(AVG(expense)"+strSpecSymbol+strSpecSymbol+"numeric,2) as expense,ROUND(AVG(profit)::numeric,2) as profit from purpose where par_row_id = :fins_project_id group by id\n", nativeQuery = true)
     List<Purpose> GetAllByProject (@Param("fins_project_id") Integer finsprojectid);
 }
