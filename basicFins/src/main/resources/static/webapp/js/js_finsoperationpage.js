286f34c9689c1a56b7e30bee54ebe1295ee0e0cd
MkTverHub <vladimijump26@gmail.com>
2022-01-22 23:40:26 +0300

    22.01.2021 Баги

--- basicFins/src/main/resources/static/webapp/js/js_finsoperationpage.js
+++ basicFins/src/main/resources/static/webapp/js/js_finsoperationpage.js
@@ -146,6 +146,7 @@ function ResetFinsOperation(){
 
 //Событие нажатия на кнопку "Сохранить" финансовую операцию
 function SaveFinsOperation(){
+    doAjaxFinsOperation()
     ClearFinsForm ();
     SetROForm();
 };
